f806c121897045312144b7a52c4e69cb26d60ddb
nzzima <nikita-krylov-2012@mail.ru>
2023-12-23 12:48:45 +0300

    Added sem1

--- sem1/HelloWorldDate.java
+++ sem1/HelloWorldDate.java
@@ -6,19 +6,18 @@ package sem1;
 import java.time.LocalDateTime;
 
 public class HelloWorldDate {
+    public static void hello() {
+        System.out.println("Hello another main!");
+    }
     public static void main(String[] args) {
+        hello();
+
         System.out.println("Hello " + "world" + "!");
         System.out.println(LocalDateTime.now()); //Date, Calendar - old versions
 
-        for (String string : args) {
-            
-        }
+        for (int i = 0; i < args.length; i++) {}
 
-        for (int i = 0; i < args.length; i++) {
-            
-        }
-
-        int[] nums = new int[6];
+        //int[] nums = new int[6];
         int[] array = {32, 32, 56, 78};
         for (int item : array) {
             System.out.print(item + " ");
