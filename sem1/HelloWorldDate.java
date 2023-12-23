dc14c4997271239ebb0675d3f4f496c123721dcf
nzzima <nikita-krylov-2012@mail.ru>
2023-12-23 12:55:37 +0300

    Added sem1 with simple examples

--- sem1/HelloWorldDate.java
+++ sem1/HelloWorldDate.java
@@ -9,16 +9,18 @@ public class HelloWorldDate {
     public static void hello() {
         System.out.println("Hello another main!");
     }
+
     public static void main(String[] args) {
         hello();
 
         System.out.println("Hello " + "world" + "!");
-        System.out.println(LocalDateTime.now()); //Date, Calendar - old versions
+        System.out.println(LocalDateTime.now()); // Date, Calendar - old versions
 
-        for (int i = 0; i < args.length; i++) {}
+        for (int i = 0; i < args.length; i++) {
+        }
 
-        //int[] nums = new int[6];
-        int[] array = {32, 32, 56, 78};
+        // int[] nums = new int[6];
+        int[] array = { 32, 32, 56, 78 };
         for (int item : array) {
             System.out.print(item + " ");
         }
