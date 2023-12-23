d8537aaeb27c96591d34d2dba65a5e13c594736f
nzzima <nikita-krylov-2012@mail.ru>
2023-12-23 12:02:27 +0300

    Create first (test) program in Java

--- program.java
+++ program.java
@@ -5,6 +5,14 @@ public class program {
 
     public static void main(String[] args) {
         System.out.println("World");
+        String str = "qwerty";
+
+        char symb = str.charAt(2);
+        System.out.println(symb);
+
+        int ind = str.indexOf('t');
+        System.out.println(ind);
+
     }
 }
 //Run like in Python or "java <name of program>"
