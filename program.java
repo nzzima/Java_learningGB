1a2c8f70970b9bba0656eb749f1941197d333d35
nzzima <nikita-krylov-2012@mail.ru>
2023-12-23 11:36:08 +0300

    Create first (test) program in Java

---
+++ program.java
@@ -0,0 +1,10 @@
+/**
+ * program
+ */
+public class program {
+
+    public static void main(String[] args) {
+        System.out.println("World");
+    }
+}
+//Run like in Python or "java <name of program>"
