4de8cfdca1a230377577c642348ec9d0d85a4e27
nzzima <nikita-krylov-2012@mail.ru>
2024-01-20 10:27:41 +0300

    Little fixed

--- sem4/compare_lists.java
+++ sem4/compare_lists.java
@@ -1,3 +1,5 @@
+package sem4;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 
--- sem4/console_stack_queue.java
+++ sem4/console_stack_queue.java
@@ -1,3 +1,5 @@
+package sem4;
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
