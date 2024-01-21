53366971146390aeba85f0e15056ddcba188965e
nzzima <nikita-krylov-2012@mail.ru>
2024-01-21 11:14:44 +0300

    Added class cat and usage

---
+++ sem6/Cat.java
@@ -0,0 +1,46 @@
+package sem6;
+
+public class Cat {
+    private String name;
+    private int age;
+    private String color;
+
+    public Cat(String color, int age, String name) {
+        this.color = color;
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
---
+++ sem6/MainCat.java
@@ -0,0 +1,14 @@
+package sem6;
+
+public class MainCat {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("черный", 2, "Шипа");
+        Cat cat2 = new Cat("белый", 5, "Джеся");
+        Cat cat3 = new Cat("серый", 1, "Барсик");
+
+        System.out.println(cat1);
+        System.out.println(cat2);
+        System.out.println(cat3);
+    }
+
+}
