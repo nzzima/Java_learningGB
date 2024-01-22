f0ac47a9a971bb85a6547f3cfc9dd5445ba7fa79
Nikita Krylov <nikita-krylov-2012@mail.ru>
2024-01-22 10:38:42 +0300

    Added _find_ and _get_ methods to PhoneBook

--- sem5hw/PhoneBook.java
+++ sem5hw/PhoneBook.java
@@ -7,12 +7,18 @@ public class PhoneBook {
     private static HashMap<String, ArrayList<Integer>> phoneBook = new HashMap<>();
 
     public static void main(String[] args) {
+        System.out.println(getPhoneBook());
         add("Nick", 12345);
         add("Kate", 12365);
         add("Bruno", 92345);
-        System.out.println(phoneBook);
+        System.out.println(getPhoneBook());
         add("Bruno", 9232345);
-        System.out.println(phoneBook);
+        System.out.println(getPhoneBook());
+        ArrayList<Integer> founded1 = find("Bruno");
+        System.out.println(founded1);
+        ArrayList<Integer> founded2 = find("Lacy");
+        System.out.println(founded2);
+        System.out.println(getPhoneBook());
     }
 
     public static void add(String name, Integer phoneNum) {
@@ -27,13 +33,16 @@ public class PhoneBook {
         }
     }
 
-    // public ArrayList<Integer> find(String name) {
+    public static ArrayList<Integer> find(String name) {
+        ArrayList<Integer> empty = new ArrayList<>();
+        if (phoneBook.containsKey(name)) {
+            return phoneBook.get(name);
+        } else {
+            return empty;
+        }
+    }
 
-    // }
-
-    // public static HashMap<String, ArrayList<Integer>> getPhoneBook() {
-
-
-    // }
-
+    public static HashMap<String, ArrayList<Integer>> getPhoneBook() {
+        return phoneBook;
+    }
 }
